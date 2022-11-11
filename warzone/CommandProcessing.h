d2ed33a0e6fee538ad05df6ffd0e44221fcdd087
Yushanai <98970042+Yushanai@users.noreply.github.com>
2022-11-10 22:59:57 -0500

    Update CommandProcessing.h

--- warzone/CommandProcessing.h
+++ warzone/CommandProcessing.h
@@ -3,22 +3,28 @@
 using std::vector;
 #include <string>
 using std::string;
-
-class CommandProcessor {
+#include "LoggingObserver.h"
+class CommandProcessor : public ILoggable, public Subject {
 public:
 	void getCommand();
+	
+	//Define stringToLog method from abstract base class ILoggable
+	string stringToLog();
 private:
 	bool validate(string command);
 	void readCommand();
 	vector<Command*> lc;
 };
 
-class Command {
+class Command : public ILoggable, public Subject{
 public:
 	Command(string Command);
 	void sideEffect();
 	void saveCommand(string command);
 	void saveEffect(string effect);
+	
+	//Define stringToLog method from abstract base class ILoggable
+	string stringToLog();
 private:
 	string command;
 	string effect;
