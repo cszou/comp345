e9a8c2b38f61af6ed1f1da97bc868965b366f5b1
Torres-Chan <86927717+Torres-Chan@users.noreply.github.com>
2022-11-12 12:33:46 -0500

    Update Player.h

--- warzone/Player.h
+++ warzone/Player.h
@@ -15,23 +15,23 @@ class Player {
 
 public:
     Player();//Default constructor
-    Player(string name); //constructor with name
-    Player(vector<Territory*> territories, Hand* hand, string name, OrderList* orderList);//Constructor 3 params
+    Player(vector<Territory*> territories, Hand* hand, string name);//Constructor 3 params
     ~Player();//Desturctor
     Player(const Player&);//Copy constructor
     Player& operator =(const Player& p); //assigment operator
     void addOrder(Order* o);
     vector<Territory*> toAttack();
     vector<Territory*> toDefend();
-    void issueOrder();
+    void issueOrder(Order* o);
     void printOrder();
-    OrderList* getlist();
+    vector<Order*> getlist();
     Hand* gethandofcard();
     bool ownsTerritory(Territory* territory);//add
+    vector<Player*> attackban; 
 private:
     vector<Territory*> territories;
     Hand* handOfCards;
-    OrderList* orderList;
+    vector<Order*> orderList;
     string name; 
     friend std::ostream & operator << (ostream &os, Player &p1);
 };
