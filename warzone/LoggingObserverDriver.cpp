4a180ac4fa09911f444202e91ba16fe38c8b5939
Yushanai <98970042+Yushanai@users.noreply.github.com>
2022-11-12 21:50:29 -0500

    Update LoggingObserverDriver.cpp

--- warzone/LoggingObserverDriver.cpp
+++ warzone/LoggingObserverDriver.cpp
@@ -2,37 +2,54 @@
 #include "LoggingObserver.h"
 #include "Map.h"
 #include "Player.h"
-#include "LoggingObserverDriver.h"
 #include "CommandProcessing.h"
 #include "GameEngine.h"
 #include "Orders.h"
 #include "Card.h"
-//using namespace std;
+#include "loggableObject.h"
+using namespace std;
 
 void testLoggingObserver(){
-     cout<< "----------------------------------------------------------------\nTesting For Part 5"<<endl;
+cout<<"----------------------Part 5 Driver----------------------------------------------"<<endl;
     //Create a log observer object
     LogObserver *ob = new LogObserver();
-	/*
-    //Create ILoggable objects for testing
-    CommandProcessor* commandprocessor = new CommandProcessor();
-   // Command command = new  Command();	
+//--------------------------GameEngine------------------------------------------
+    //Create Gameengine object and change it state, so the Notify method will be invoked
     GameEngine* engine = new GameEngine();
-	
-    //Attach each object to the observer object
-    commandprocessor -> Attach(ob);
     engine -> Attach(ob);
-    //command  -> Attach(ob);
-	
     //Testing the GameEngine, when game engine change its state, the new state will be written to the log file
+    cout<<"Changing Game engine's state, updated string will be updated to file "<<endl;
     engine->setState("Start");
     engine->setState("Map Loaded");
     engine->setState("Assign Reinforcement");
-    engine->setState("Win");	
-    */
-    //Testing the CommandProcessor, when a command is read, it is written in the log file
-    //Testing the Order 
-	
+    engine->setState("Win");
+
+//-----------------------CommandProcessor&Command---------------------------------------------
+    //Create CommandProcessor objects for testing
+    GameEngine *game  = new GameEngine();
+	// read from console
+	CommandProcessor* cp = new CommandProcessor(game);
+    cp -> Attach(ob);
+	Command* c1 = cp->getCommand();
+    c1 -> Attach(ob);
+    c1->saveEffect();
+	cp->validate(c1);
+	// read from file
+	CommandProcessor* fcp = new FileCommandProcessorAdapter(game);
+    fcp->Attach(ob);
+	Command* c2 = fcp->getCommand();
+    c2 ->Attach(ob);
+    c2->saveEffect();
+   
+	fcp->validate(c2);
+	Command* c3 = fcp->getCommand();
+    c3->Attach(ob);
+    c3->saveEffect();
+	fcp->validate(c3);
+}
+   
+//--------------------Order&OrderList------------------------------------------------
+/*
     // create  Territory object
     int NUMBEROFARMY =13;
     Territory* A1= new Territory("A1");
@@ -55,7 +72,7 @@ void testLoggingObserver(){
     //create each order objects
     Deploy* deploy = new Deploy(&NUMBEROFARMY,P1,A1);
     Advance* advance= new Advance(A1,A2,P1,&NUMBEROFARMY);
-    Airlift* airlift= new Airlift(P1, A1,A2); 
+   // Airlift* airlift= new Airlift(P1, A1,A2); 
     Bomb* bomb = new Bomb (P1, A1,&NUMBEROFARMY); 
     Blockade* blockade= new Blockade(P1,A1); 
     //Order* negotiate= new Negotiate(P1,P2);
@@ -63,7 +80,7 @@ void testLoggingObserver(){
     //Attach each object to the observer object
     deploy -> Attach(ob);
     advance -> Attach(ob);
-    airlift -> Attach(ob);
+    //airlift -> Attach(ob);
     bomb -> Attach(ob);
     blockade -> Attach(ob);
     //negotiate -> Attach(ob);
@@ -71,7 +88,7 @@ void testLoggingObserver(){
     //Testing Order execute method, the exceuted order will be outputed to log file
     deploy->execute();
     advance->execute();
-    airlift->execute();
+    //airlift->execute();
     bomb->execute();
     blockade->execute();
     //negotiate->execute();
@@ -82,11 +99,11 @@ void testLoggingObserver(){
     //Testing orderlist addOrder, the added order will be outputed to log file
     testOrderList -> addOrders(deploy);
     testOrderList -> addOrders(advance);
-    testOrderList -> addOrders(airlift);
+    //testOrderList -> addOrders(airlift);
     testOrderList -> addOrders(bomb);
     testOrderList -> addOrders(blockade); 
     //testOrderList -> add(negotiate);
-	
+//--------------------------Deleting Pointers------------------------------------------
   //Deleting pointers to prevent memory leak 
    // delete ob;
     //delete commandprocessor;
@@ -94,35 +111,4 @@ void testLoggingObserver(){
    // delete A1, A2;
    // delete deploy, advance, airlift, bomb, blockade ;//negotiate;
 	
-}
-/*
-int main(){
-    void testLoggingObserver();
-}
-
-
-
-Method implement in Iloggable classes
-
-string Orders :: stringToLog(){
-    return "Order Excecuted: "<< ;
-}
-string OrderList :: stringToLog(){
-    string OrderList = "Order Issued: "
-    return OrderList;
-}
-string GameEngine::stringToLog() {
-    string GameEngine = "Game Engine New State: " + getState();
-    return GameEngine
-}
-string CommandProcessor::stringToLog(){
-	return "Command: " + commandList.back()->getCommandStr() + " into the list.";
-}
-
-string Command::stringToLog() {
-	string command = "Command issued: " + getCommandStr()
-		+ "\nCommand's effect: " + getEffect();
-	return "Command issued: "<< getEffect();
-}
-
-*/
+}*/
