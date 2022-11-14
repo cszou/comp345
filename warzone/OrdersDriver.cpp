168fd5a427bf599a43ae62024032d77d11ed55f6
Torres-Chan <86927717+Torres-Chan@users.noreply.github.com>
2022-11-13 19:39:20 -0500

    Update OrdersDriver.cpp

--- warzone/OrdersDriver.cpp
+++ warzone/OrdersDriver.cpp
@@ -1,144 +1,109 @@
 #include <iostream>
-#include<vector>
-#include<string>
-#include"Orders.h"
-#include"Player.h"
-#include"Map.h"
+#include <vector>
+#include <string>
+#include "Orders.h"
+#include "Player.h"
+#include "Map.h"
 #include "Card.h"
+using namespace std;
 
-void testOrdersLists() {
+void testOrderExecution()
+{
+    //    std::cout << "ass big aiyushan" << std::endl;
+    int NUMBEROFARMY = 10; // add army
+    Territory *A1 = new Territory("A1");
+    int ownarmy = 150;
+    int targetarmy = 200;
+    A1->setNumberOfArmies(targetarmy);
+    Territory *A2 = new Territory("A2");
+    A2->setNumberOfArmies(ownarmy);
+    OrderList *OL1 = new OrderList();
+    OrderList *OL2 = new OrderList();
+    vector<Territory *> T1;
+    T1.push_back(A2);
+    vector<Territory *> T2;
+    T2.push_back(A1);
+    Hand *H = new Hand();
+    Player *P1 = new Player(T1, H, "P1", OL1);
+    Player *P2 = new Player(T2, H, "P2", OL2);
+    // creating orders:
+    //*************************************************
+    // need to set the owner of the terotery.
+    A2->setOwner(P1);
+    A1->setOwner(P2);
+    //#1 deploy
+    // 8888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
 
-	//std::cout << "\nPart 3: Orders list" << std::endl;
-	//bool check = true;
-	//int orderfind;
-	//OrderList* k = new OrderList();
-	//std::cout << *k << std::endl;//this is a list of orders
-	//while (check) {
-	//	std::cout << "which order you want to create? 1.advance;2.deploy;3.bomb;4.blockade;5.airlift;6.negotiate;7.non" << std::endl;
-	//	std::cin >> orderfind;
-	//	switch (orderfind) {
-	//	case 1:
-	//	{
-	//		Advance* jd = new Advance();
-	//		std::cout<<jd;
-	//		jd->validate(jd);//description needs,stream insertion operator for each sub class.
-	//		k->add(jd);
-	//		break;
-	//	}
-	//	case 2:
-	//	{
-	//		Deploy* jd = new Deploy();
-	//		std::cout<<jd;
-	//		jd->validate(jd);
-	//		k->add(jd);
-	//		break;
-	//	}
-	//	case 3:
-	//	{
-	//		Bomb* jd = new Bomb();
-	//		std::cout<<jd;
-	//		jd->validate(jd);
-	//		k->add(jd);
-	//		break;
-	//	}
-	//	case 4:
-	//	{
-	//		Blockade* jd = new Blockade();
-	//		std::cout<<jd;
-	//		jd->validate(jd);
-	//		k->add(jd);
-	//		break;
-	//	}
-	//	case 5:
-	//	{
-	//		Airlift* jd = new Airlift();
-	//		std::cout<<jd;
-	//		jd->validate(jd);
-	//		k->add(jd);
-	//		break;
-	//	}
-	//	case 6:
-	//	{
-	//		Negotiate* jd = new Negotiate();
-	//		std::cout<<jd;
-	//		jd->validate(jd);
-	//		k->add(jd);
-	//		break;
-	//	}
-	//	case 7:
-	//	{
-	//		check = false;
-	//		break;}
-	//	}
-	//}
-	//std::cout << "number of cards : " << k->listsize() << std::endl;
-	//for (int i = 0;i < k->listsize();i++) {
-	//	std::cout << k->invoke(i) << std::endl;
-	//}
-	//OrderList* k1 = new OrderList(*k);//copy constructor
-	//std::cout << "the order of original pointer:" << k << std::endl;
-	//std::cout << "the order of deep copyed pointer:" << k1 << std::endl;
-
-	//std::cout << "checking execution method...." << std::endl;
-	//for (int i = 0;i < k1->listsize();i++) {
-	//	k1->invoke(i)->execute(k1->invoke(i));
-	//}
-	//bool cont = true;//remove method
-	//	std::cout << "delete which one?";
-	//	int threa;
-	//	while (cont) {
-	//		std::cin >> threa;
-	//		if (threa > k1->listsize())
-	//			std::cout << "Out of bound. Please try again." << std::endl;
-	//		else
-	//			cont = false;
-	//	}
-	//	k1->removal(threa);
-	//	std::cout << "after cancellization:" << std::endl;
-	//	for (int i = 0;i < k1->listsize();i++) {
-	//		std::cout << k1->invoke(i) << std::endl;
-	//	}
-	//int old;
-	//int new1;
-	//cont = true;
-	//while(cont) {
-	//	std::cout << "move which one to where?";
-	//	std::cin >> old;
-	//	std::cout << "move " << old << " to ? ";
-	//	std::cin >> new1;
-	//	std::cout << "move " << old << " to " << new1 << std::endl;
-	//	if(old>k1->listsize()||new1>k1->listsize())
-	//		std::cout << "Out of bound. Please try again." << std::endl;
-	//	else
-	//		cont = false;
-	//}
-	//k1->move(old, new1);
-	//for (int i = 0;i < k1->listsize();i++) {//move method 
-	//	std::cout << k1->invoke(i) << std::endl;
-	//}
-	//delete k1;
-	//k1 = NULL;//avoid memory leaks
-	//std::cout << "Orders list demo end." << std::endl << std::endl;
+    //***********when TERROR NOT BELOG TO THE PLAYER
+        Deploy *deploy = new Deploy(NUMBEROFARMY, P2, A2);
+        OL1->addOrders(deploy);
+        deploy->execute();
+        deploy->~Deploy();
+    //**********when belongs to
+        std::cout << "*************Deploy class******************"<<std::endl;
+        Deploy *deploy = new Deploy(NUMBEROFARMY, P1, A2);
+        OL1->addOrders(deploy);
+        std::cout << "*****Execution method*****"<<std::endl;
+        deploy->execute();
+        std::cout << "insertion operator : " <<deploy<<endl;
+        std::cout << "Copy constructor operator : "<<endl;
+        Deploy *hello = deploy;
+        hello->execute();
+        std::cout << "String to log : "<<hello->stringToLog()<<endl;
+       hello->~Deploy();
+    //#2 advance
+    // 8888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+    
+    std::cout << "*************Advance class******************" << std::endl;
+    Advance *advance = new Advance(A2, A1, P1, NUMBEROFARMY);
+    Advance *fe = new Advance(*advance);
+    std::cout << fe->getName() << std::endl;
+    std::cout << advance->getName() << std::endl;
+    std::cout << "*****************************" << std::endl;
+    std::cout << "number of terriotories in the Player: " << P1->getTerriotory().size() << std::endl;
+    fe->execute();
+    std::cout << "Name of target terory now belongs to : " << A1->getOwner()->getname() << std::endl;
+    std::cout << "***outcome***" << std::endl;
+    std::cout << "own tero previously has: " << ownarmy << ",now has: " << A2->getNumberOfArmies() << std::endl;
+    std::cout << "target tero previously has: " << targetarmy << ",conquer tero now has: " << A1->getNumberOfArmies() << std::endl;
+    std::cout << "number of terriotories in the Player: " << P1->getTerriotory().size() << std::endl;
+    
+    // 8888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+    
+    std::cout << "*************Airlift*************" << endl;
+    Airlift *airlift = new Airlift(P1, A1, A2);
+    Airlift *fe = new Airlift(*airlift); // cannot make it
+    std::cout << &airlift << endl;
+    std::cout << &fe << endl;
+    A1->setOwner(P1);
+    airlift->execute();
+    
+    // 8888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+    
+        std::cout << "*************Bomb*************" << endl;
+        Bomb *bomb = new Bomb(P1, A1, &NUMBEROFARMY);
+        A1->addNeighbour(A2);
+        std::cout << "Neighbours of A1: ";
+        A1->showNeighbours();
+        std::cout << "Neighbours of A2: ";
+        A2->showNeighbours();
+        bomb->execute();
+    
+    // 8888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+    
+     std::cout << "*************Bomb*************" << endl;
+          Blockade* blockade= new Blockade(P1,A1);
+         blockade->execute();
+     
+    // 8888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+    std::cout << "*************Bomb*************" << endl;
+    Negotiate *negotiate = new Negotiate(P1, P2);
+    negotiate->execute();
+    std::cout<<"The attackban list of P1" <<P1->attackban.at(0)->getname()  << endl;
+     //g++ -std=c++11  Orders.cpp Player.cpp Card.cpp Orderdriver.cpp Map.cpp
+     
 }
-
-void testOrderExecution() {
-	int NUMBEROFARMY = 13;
-	//Creating objects for testing  
-//    Territory* A1= new Territory("A1
-	Territory* A2 = new Territory("A2");
-	OrderList* OL1 = new OrderList();
-	vector <Territory*> T1;
-	//   T1.push_back(A1);
-	T1.push_back(A2);
-	Hand* H = new Hand();
-	Player* P1 = new Player(T1, H, "P1", OL1);
-	// creating orders:
-	Deploy* deploy = new Deploy(&NUMBEROFARMY, P1, A2);
-	/*    Advance* advance= new Advance(A1,A2,P1,&NUMBEROFARMY);
-		Airlift* airlift= new Airlift(P1, A1,A2);
-		Bomb* bomb = new Bomb (P1, A1,&NUMBEROFARMY);
-		Blockade* blockade= new Blockade(P1,A1);
-		Order* negotiate= new Negotiate(P1,P2);
-	*/
-
+int main()
+{
+    testOrderExecution();
 }
