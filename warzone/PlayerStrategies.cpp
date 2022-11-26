9dababdad6c5f4db11c93bb429c874e850b0aacb
Yushanai <98970042+Yushanai@users.noreply.github.com>
2022-11-26 15:40:22 -0500

    Update PlayerStrategies.cpp

--- warzone/PlayerStrategies.cpp
+++ warzone/PlayerStrategies.cpp
@@ -1,9 +1,37 @@
 #include "PlayerStrategies.h"
+#include "Player.h"
+#include <iostream>
+#include <string>
+using namespace std;
 
-//HumanPlayer
-//--------------------------------------------------------------
+//PlayerStrategy Base Class
+//--------------------------------------------------------------------
+PlayerStrategy::PlayerStrategy(Player* player){
+    this->p = player;
+}
+
+//NeutralPlayerStrategy
+//--------------------------------------------------------------------
+NeutralPlayerStrategy::NeutralPlayerStrategy(string strategy_Name,Player* player):PlayerStrategy (player){
+    strategyName = strategy_Name;
+}
+string NeutralPlayerStrategy::getStrategyName(){
+    return this->strategyName;
+}
+
+void NeutralPlayerStrategy:: issueOrder(string orderName){
+
+    cout<<"Excecuting isssue order from "<<getStrategyName()<<endl;
 
 
 
 
 
+}
+vector<Territory*> NeutralPlayerStrategy::toAttack(){
+
+}
+vector<Territory*> NeutralPlayerStrategy::toDedend(){
+    
+}
+//--------------------------------------------------------------------
