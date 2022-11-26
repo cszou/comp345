6a549517eb54f128f6f227960e326c407d4c9938
Yushanai <98970042+Yushanai@users.noreply.github.com>
2022-11-26 16:27:36 -0500

    Update PlayerStrategies.cpp

--- warzone/PlayerStrategies.cpp
+++ warzone/PlayerStrategies.cpp
@@ -5,15 +5,21 @@
 using namespace std;
 
 //PlayerStrategy Base Class
-//--------------------------------------------------------------------
+//----------------------------------------------------------
 PlayerStrategy::PlayerStrategy(Player* player){
     this->p = player;
 }
 
+//AggressivePlayerStrategy 
+//----------------------------------------------------------
+AggressivePlayerStrategy::AggressivePlayerStrategy(Player* player):PlayerStrategy (player){
+    strategyName = "AggressivePlayer";
+}
+
 //NeutralPlayerStrategy
-//--------------------------------------------------------------------
-NeutralPlayerStrategy::NeutralPlayerStrategy(string strategy_Name,Player* player):PlayerStrategy (player){
-    strategyName = strategy_Name;
+//----------------------------------------------------------
+NeutralPlayerStrategy::NeutralPlayerStrategy(Player* player):PlayerStrategy (player){
+    strategyName ="Neutral Player";
 }
 string NeutralPlayerStrategy::getStrategyName(){
     return this->strategyName;
@@ -22,7 +28,9 @@ string NeutralPlayerStrategy::getStrategyName(){
 void NeutralPlayerStrategy:: issueOrder(string orderName){
 
     cout<<"Excecuting isssue order from "<<getStrategyName()<<endl;
-
+    
+    //when NeutralPlayer is attacked, it becomes AggressivePlayer
+    p->setPlayerStrategy(new AggressivePlayerStrategy(p));
 
 
 
@@ -34,4 +42,4 @@ vector<Territory*> NeutralPlayerStrategy::toAttack(){
 vector<Territory*> NeutralPlayerStrategy::toDedend(){
     
 }
-//--------------------------------------------------------------------
+//----------------------------------------------------------
