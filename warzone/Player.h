c7eb7c46f409b97d43807dba532cfef820927c43
Torres-Chan <86927717+Torres-Chan@users.noreply.github.com>
2022-11-28 22:11:43 -0500

    Update Player.h

--- warzone/Player.h
+++ warzone/Player.h
@@ -1,4 +1,4 @@
-#pr#pragma once
+#pragma once
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -31,86 +31,6 @@ public:
     vector<Territory *> toDefend();
     void printOrder();
     void issueOrder(string orderName);
-    OrderList *getlist();
-    string getName();
-    Hand *gethandofcard();
-    bool ownsTerritory(Territory *territory); // add
-    vector<Player *> attackban;
-    vector<Territory *> getTerriotory();
-    void addTerritory(Territory *o);
-    // Newly added methods
-    void setifattected();
-    bool getifattacked();
-    void setTerritories(vector<Territory *> &t);
-    void setReinforcement(int reinforcement);
-    int getReinforcement();
-    bool assignReinforcement(int num);
-    void set_Deploy_Territories();
-    void set_Available_Territories();
-    void clear_Deploy_Territories();
-    void clear_Available_Territories();
-    void set_all_territories(vector<Territory *> all);
-    void set_players_Map(vector<Player *> players);
-
-    // Newly added method for part 3
-    void setPlayerStrategy(PlayerStrategy *newPlayerStrategy);
-    Player(PlayerStrategy *ps);
-    std::map<string, Territory *> getDeploy_territories();
-    std::map<string, Territory *> getAvailable_territories();
-    std::map<string, Player *> getPlayers_Map();
-    vector<Player *> getAttackBan();
-
-    void  deleteTerriotory(Territory *o);
-
-private:
-    vector<Territory *> territories;
-    Hand *handOfCards;
-    OrderList *orderList;
-    string name;
-    friend std::ostream &operator<<(ostream &os, Player &p1);
-    bool whetherattacked;
-    // Newly added variables
-    int reinforcement;
-    std::map<string, Territory *> deploy_territories;
-    std::map<string, Territory *> available_territories;
-    std::map<string, Territory *> all_territories;
-    vector<Player *> attackBan;
-    std::map<string, Player *> players_Map;
-    // Newly added for Player Strategy
-    PlayerStrategy *ps;
-};agma once
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <vector>
-#include <map>
-#include <sstream>
-#include <algorithm>
-#include "Map.h"
-#include "Card.h"
-#include "Orders.h"
-
-using namespace std;
-class Command;
-class Territory;
-class Order;
-class OrderList;
-class Hand;
-class PlayerStrategy;
-class Player
-{
-public:
-    Player();                                                                               // Default constructor
-    Player(string name);                                                                    // constructor with name
-    Player(vector<Territory *> territories, Hand *hand, string name, OrderList *orderList); // Constructor 3 params
-    ~Player();                                                                              // Desturctor
-    Player(const Player &);                                                                 // Copy constructor
-    Player &operator=(const Player &p);                                                     // assigment operator
-    void addOrder(Order *o);
-    vector<Territory *> toAttack();
-    vector<Territory *> toDefend();
-    void printOrder();
-    bool issueOrder(string orderName);
     OrderList *getlist();
     string getName();
     Hand *gethandofcard();
